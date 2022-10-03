f75a02044d69f4474383933bed6252730149d753
majed <maged.soft@hotmail.com>
2022-10-04 03:59:22 +0400

    class for email

---
+++ app/Mail/SendEmailDemo.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmailDemo extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public $view;
+    public $title;
+    public $text;
+    public $subject;
+
+    public function __construct($view,$subject,$title,$text)
+    {
+        $this->subject = $subject;
+        $this->title = $title;
+        $this->text = $text;
+        $this->view = $view;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject($this->subject)
+            ->view($this->view);
+    }
+}
